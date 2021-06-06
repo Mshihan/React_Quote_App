7ec08e971f48401258230332bd893e6c1069830c
Mshihan <167sihan@gmail.com>
2021-06-06 18:16:10 +0530

    not working

--- src/Pages/NewQuotes.js
+++ src/Pages/NewQuotes.js
@@ -1,10 +1,14 @@
+import { useHttp } from "./../hooks/use-http";
 import QuoteForm from "../components/quotes/QuoteForm";
 import { useHistory } from "react-router";
+import { addQuote } from "./../lib/api";
 
 const NewQuote = () => {
   const history = useHistory();
+  const { sendRequest, httpState } = useHttp(addQuote);
   const onAddQuoteHandler = (quotes) => {
     console.log(quotes);
+
     history.push("/quotes");
   };
   return <QuoteForm onAddQuote={onAddQuoteHandler} />;
