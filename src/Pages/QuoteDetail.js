13611fe01a957a4b9c880570e89b75a33b6d6819
Mshihan <167sihan@gmail.com>
2021-06-06 13:27:41 +0530

    My solution with nested route

--- src/Pages/QuoteDetail.js
+++ src/Pages/QuoteDetail.js
@@ -1,6 +1,7 @@
-import { Route, useParams } from "react-router";
+import { Route, useHistory, useParams } from "react-router";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "./../components/quotes/HighlightedQuote";
+import { useState } from "react";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Shihan", text: "Learning React is fun!" },
@@ -9,6 +10,8 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
+  const history = useHistory();
+  const [showingComments, setShowingComments] = useState(false);
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
@@ -16,9 +19,22 @@ const QuoteDetail = () => {
     return <p>No Quote Found!</p>;
   }
 
+  const onClickShowCommentHander = () => {
+    if (!showingComments) {
+      setShowingComments(true);
+      history.push(`/quotes/${quote.id}/comments`);
+    } else {
+      setShowingComments(false);
+      history.push(`/quotes/${quote.id}`);
+    }
+  };
+
   return (
     <>
       <HighlightedQuote author={quote.author} text={quote.text} />
+      <button onClick={onClickShowCommentHander}>
+        {showingComments ? "Hide Comments" : "Show Comments"}{" "}
+      </button>
       {/* <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route> */}
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,39 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const urlParams = new URLSearchParams(location.search);
+  const isSortingAscending = urlParams.get("sort") === "asc";
+
+  const quoteList = sortQuotes(props.quotes, isSortingAscending);
+  const onClickSortingHandler = () => {
+    history.push("/quotes?sort=" + (isSortingAscending ? "dsc" : "asc"));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={onClickSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {quoteList.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
